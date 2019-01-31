2e89246d7d2a536c73d51e6c66f9ee9cc320bb68
Gabby202 <gabbu202@gmail.com>
2019-01-31 20:32:58 +0000

    3d infinite runner started

---
+++ Assets/Scripts/BallController3D.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController3D : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed = 10;
+    public Vector3 playerMovement;
+    private float turnForce = 100;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void MoveBasedOnHeadBinary(float h)
+    {
+        Vector3 direction = new Vector3(h, 0f, 0f);
+        rb.AddForce(direction * speed);
+    }
+
+    public void MoveBasedOnKeyboard(float h)
+    {
+        Vector3 direction = new Vector3(h, 0f, 0f);
+        rb.AddForce(direction * speed);
+    }
+
+    public void StopBall()
+    {
+        rb.velocity = Vector3.zero;
+    }
+
+    public void MoveBasedOnHeadRange(float h)
+    {
+        // uses direct translation of head tilt, and * by 1000 to move rb
+        //if (h > 0.05f || h < -0.05f)
+        //{
+        //    playerMovement = new Vector3(-h, 0.0f, 0.0f) * speed * Time.deltaTime;
+        //    Teleport(h);
+        //}
+        //else
+        //{
+        //    playerMovement = new Vector3(0f, 0f, 0f) * speed * Time.deltaTime;
+        //}
+        Vector3 direction = new Vector3(h, 0f, 0f);
+        rb.AddForce(direction * speed);
+
+        //rb.MovePosition(playerMovement * 1000f);
+        // transform.Translate((target.position - transform.position) * Time.deltaTime * speed);
+    }
+
+    private void Teleport(float x)
+    {
+        x = -x * 200;
+        //print("x = " + x);
+        float y = transform.position.y;
+        float z = transform.position.z;
+        float oldX = transform.position.x;
+
+        x = Mathf.Lerp(oldX, x, 0.1f);
+
+        Vector3 newPos = new Vector3(x, y, z);
+
+        transform.position = newPos;
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        rb.AddForce(0, 0, speed);
+	}
+}
---
+++ Assets/Scripts/GameController3D.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController3D : MonoBehaviour {
+
+	private Gaze myGazeGetter;
+	private BallController3D ballController;
+	private float movementHorizontal;
+	public int configuration = 2;
+	//public GameObject prefab;
+	// Use this for initialization
+	void Start () {
+		myGazeGetter = FindObjectOfType<Gaze>();
+		ballController = FindObjectOfType<BallController3D>();
+        //every 20th of a second
+        SetConfiguration(this.configuration);
+
+	}
+	private void SetConfiguration(int configuration)
+    {
+        switch (configuration)
+        {
+            case 0:
+                InvokeRepeating("BallMoverBinary", 0, 0.0125f);
+                break;
+            case 1:
+                InvokeRepeating("BallMoverRange", 0, 0.0125f);
+                break;
+            case 2:
+                InvokeRepeating("BallMoverKeyboard", 0, 0.0125f);
+                break;
+            default:
+                InvokeRepeating("BallMoverBinary", 0, 0.0125f);
+                break;
+
+        }
+    }
+	public void BallMoverBinary() 
+	{
+	
+		// Debug.Log("Head tilt value: " + myGazeGetter.gazeDataObject.head);
+		// Debug.Log("Tilting left: " + myGazeGetter.IsTiltingLeft() + "\nTilting Right: " + myGazeGetter.IsTiltingRight());
+		movementHorizontal = 0;
+		if(myGazeGetter.IsTiltingLeft()) 
+		{
+			// Debug.Log("left");
+			movementHorizontal = -1;
+		} else if(myGazeGetter.IsTiltingRight()) 
+		{
+			// Debug.Log("right");
+			movementHorizontal = 1;
+
+		}
+		ballController.MoveBasedOnHeadBinary(movementHorizontal);
+	}
+
+	public void BallMoverRange()
+	{
+        
+		//Debug.Log("Head tilt value: " + myGazeGetter.gazeDataObject);
+		movementHorizontal = myGazeGetter.GetHeadTiltValue();
+		ballController.MoveBasedOnHeadRange(movementHorizontal);
+	}
+
+    public void BallMoverKeyboard()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            movementHorizontal = -1;
+            ballController.MoveBasedOnKeyboard(movementHorizontal);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            movementHorizontal = 1;
+            ballController.MoveBasedOnKeyboard(movementHorizontal);
+        }
+        //else if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        //{
+        //    ballController.StopBall();
+        //}
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            if (this.configuration == 0 || this.configuration == 1)
+            {
+                this.configuration++;
+            }
+            else
+            {
+                this.configuration = 0;
+            }
+            this.SetConfiguration(configuration);
+            Debug.Log("Configuration Changed to CONFIG: " + this.configuration );
+        }
+    }
+
+}
